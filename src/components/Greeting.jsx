aa49b9cb51ef2ef473835e54ad8d671c0bdf48b9
Wenura Kavinda <wenurarcc@gmail.com>
2026-05-01 22:34:53 +0530

    add some final touches

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,7 +3,7 @@ function Greeting() {
     <section className="greeting">
       <p className="eyebrow">React JS Lab 01</p>
       <h1>Hello World!</h1>
-      <p>Welcome to my first React app built with Vite.</p>
+      <p>Welcome to my React app built with Vite (But this isn't my 1st one or last one).</p>
     </section>
   )
 }
